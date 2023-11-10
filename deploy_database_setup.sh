7b6fa041ce7c556b137acfc8747b46f7b61e6721
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-11-10 22:01:46 +0100

    Deployed

--- deploy_database_setup.sh
+++ deploy_database_setup.sh
@@ -5,10 +5,10 @@ PODNAME=$(a=$(kubectl get pods | grep 'mariadb' | grep 'Running' | awk '{print $
 
 echo "Setting up MySQL at" $PODNAME "cluster"
 # Copy setup sql files to database cluster
-kubectl cp ./setup_mariadb.sql olagoldhackxx-dev/"$PODNAME":/tmp/setup_mariadb.sql
+kubectl cp ./setup_mariadb.sql climatewavers-dev/"$PODNAME":/tmp/setup_mariadb.sql
 echo "Copied setup sql file to $PODNAME:/tmp/setup_mariadb.sql on openshift cluster"
 # Copy setup scripts to database cluster
-kubectl cp ./setup_database.sh olagoldhackxx-dev/"$PODNAME":/tmp/setup_database.sh
+kubectl cp ./setup_database.sh climatewavers-dev/"$PODNAME":/tmp/setup_database.sh
 echo "Copied setup script file to $PODNAME:/tmp/setup_database.sh on openshift cluster"
 # Run script to setup database on cluster
 kubectl exec pod/$PODNAME -- /bin/bash /tmp/setup_database.sh
